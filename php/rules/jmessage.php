a237e23d2121bdab5593d2b2ea8181678664c694
hoochicken <mareike.riegel@ql.de>
2023-03-24 10:37:32 +0100

    refactor (rules) Rules have been refactored to generate a clearer code

--- php/rules/jmessage.php
+++ php/rules/jmessage.php
@@ -19,11 +19,11 @@ defined('JPATH_PLATFORM') or die;
 class JFormRuleJmessage extends JFormRule
 {
     /**
-     * Method to test the email address and optionally check for uniqueness.
+     * Method to test whether the recipient-id and the sender-id is an integer
      *
      * @param SimpleXMLElement $element The SimpleXMLElement object representing the <field /> tag for the form field object.
      * @param mixed $value The form field value to validate.
-     * @param null $group The field name group control value. This acts as as an array container for the field.
+     * @param null $group The field name group control value. This acts as an array container for the field.
      *                                      For example if the field has name="foo" and the group value is set to "bar" then the
      *                                      full field name would end up being "bar[foo]".
      * @param JRegistry|null $input An optional JRegistry object with the entire data set to validate against the entire form.
